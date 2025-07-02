e9a656cea659f077e15ee2cb2726aacd1a5257b7
Chetan Verma <coutcv28@gmail.com>
2025-07-03 00:16:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,6 @@ app.post('/', async (req, res) => {
     const findResult = await collection.insertOne(password)
     res.send({success:true, result: findResult})
 })
-// app.delete('/', async (req, res) => {
-//     const password = req.body
-//     const db = client.db(dbName)
-//     const collection = db.collection('passwords')
-//     const findResult = await collection.deleteOne(password)
-//     res.send({success:true, result: findResult})
-// })
 
 app.delete('/:id', async (req, res) => {
   const { id } = req.params
